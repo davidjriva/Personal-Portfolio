3e2f0411a45f9a7b872c0120356df933ee8e1133
David Riva <davidjriva@gmail.com>
2024-08-23 13:49:10 -0700

    update avatar icon in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,26 +6,35 @@ const pages = ['About', 'Experience', 'Education', 'Projects', 'Skills', 'Awards
 
 function NavBar() {
   return (
-    <AppBar 
-      position="fixed" 
-      sx={{ 
-        backgroundColor: '#3c4142', 
-        height: '100vh', 
-        width: '200px', 
-        left: 0, 
-        top: 0, 
-        display: 'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        padding: '1rem', 
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', 
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: '#3c4142',
+        height: '100vh',
+        width: '200px',
+        left: 0,
+        top: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '1rem',
+        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
       }}
     >
       <Toolbar disableGutters sx={{ flexDirection: 'column', alignItems: 'center', width: '100%' }}>
         <Box sx={{ mb: 3, textAlign: 'center' }}>
           <IconButton href="/about">
-            <Avatar alt="Photo of David Riva" src="/images/headshot.jpeg" sx={{ width: 80, height: 80 }} />
+            <Avatar
+              alt="Photo of David Riva"
+              src="/images/headshot.jpeg"
+              sx={{
+                width: 150,
+                height: 150,
+                boxShadow: '0 0 20px 5px rgba(211, 211, 211, 0.8)', 
+                border: '4px light gray',
+              }}
+            />
           </IconButton>
           <Typography variant="h6" sx={{ fontWeight: 700, mt: 1 }}>
             David Riva
@@ -38,9 +47,9 @@ function NavBar() {
               href={`/${slugify(page.toLowerCase())}`}
               color="inherit"
               sx={{
-                my: 1, 
+                my: 1,
                 fontWeight: 700,
-                width: '100%', 
+                width: '100%',
                 textAlign: 'center',
               }}
               key={page}
