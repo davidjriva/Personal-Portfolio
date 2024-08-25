fc2ff9617d64161ab0f09889034282b7a2a8616d
David Riva <davidjriva@gmail.com>
2024-08-25 14:30:50 -0700

    add awards Icon

--- src/components/AwardCard.js
+++ src/components/AwardCard.js
@@ -1,11 +1,15 @@
 import { Typography, Box } from '@mui/material';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 
 const AwardCard = ({ title, description, date }) => {
   return (
     <Box sx={{ marginTop: '2rem', marginBottom: '2rem' }}>
-      <Typography variant="h5" sx={{ color: '#000' }}>
-        {title}
-      </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <EmojiEventsIcon sx={{ color: '#FFD700', marginRight: '0.5rem' }} /> {/* Gold-yellow color */}
+        <Typography variant="h5" sx={{ color: '#000' }}>
+          {title}
+        </Typography>
+      </Box>
       <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>
         {description}
       </Typography>
