f8146f78c40535f5eb55d551e03e1abb633e51b9
David Riva <davidjriva@gmail.com>
2024-10-07 20:15:46 -0700

    Set font to montserrat sans-serif

--- personal-portfolio/src/theme.js
+++ personal-portfolio/src/theme.js
@@ -1,9 +1,18 @@
 'use client';
+
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'var(--font-roboto)',
+    fontFamily: ['--font-montserrat', 'sans-serif'].join(','),
+    h1: { fontWeight: 'bold' },
+    h2: { fontWeight: 'bold' },
+    h3: { fontWeight: 'bold' },
+    h4: { fontWeight: 'bold' },
+    h5: { fontWeight: 'bold' },
+    h6: { fontWeight: 'bold' },
+    body1: { fontWeight: 400 },
+    body2: { fontWeight: 400 },
   },
 });
 
