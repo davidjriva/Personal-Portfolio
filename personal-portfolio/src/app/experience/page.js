b48497df39fa456a75a22aa970088b78b00180b9
David Riva <davidjriva@gmail.com>
2024-10-07 19:39:00 -0700

    Add experience page and page data files

--- personal-portfolio/src/app/experience/page.js
+++ personal-portfolio/src/app/experience/page.js
@@ -1,3 +1,41 @@
-export default function About() {
-  return <div>Hello World!</div>;
-}
+'use client';
+
+import React from 'react';
+import { Box, Typography, Divider } from '@mui/material';
+import AsyncHelmet from '../../components/AsyncHelmet';
+import ExperienceCard from '../../components/Experience/ExperienceCard';
+import experienceData from '../../data/experiences.json';
+
+const Experience = () => {
+  const sortedExperienceData = [...experienceData].sort((a, b) => {
+    return new Date(b.startDate) - new Date(a.startDate);
+  });
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '2rem',
+        color: '#757474',
+      }}
+    >
+      <AsyncHelmet pageName="Experience" />
+
+      <Typography variant="h4" sx={{ marginBottom: 6, color: '#000', textAlign: 'center' }}>
+        Experience
+      </Typography>
+
+      {sortedExperienceData.map((experience) => (
+        <React.Fragment key={experience.title}>
+          <ExperienceCard {...experience} />
+          <Divider sx={{ marginBottom: 2 }} />
+        </React.Fragment>
+      ))}
+    </Box>
+  );
+};
+
+export default Experience;
