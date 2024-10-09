dee2a74d9421f7ad6d90a8d21351d821153ebf03
David Riva <davidjriva@gmail.com>
2024-10-08 22:15:08 -0700

    Coding refactor

--- personal-portfolio/src/components/About-Page/NewAbout.js
+++ personal-portfolio/src/components/About-Page/NewAbout.js
@@ -1,6 +1,21 @@
-import { Typography, Box } from '@mui/material';
-import SocialLinks from './SocialLinks';
-import ResumeButton from './ResumeButton';
+import { Box } from '@mui/material';
+import AboutHeader from './AboutHeader';
+import AboutFooter from './AboutFooter';
+import Biography from './Biography';
+
+const PersonOutlineIcon = () => {
+  return (
+    <img
+      src="/images/person-outline.svg"
+      alt="Person"
+      style={{
+        width: '25vw',
+        height: 'auto',
+        marginRight: '20px', // Add space between image and text
+      }}
+    />
+  );
+};
 
 const NewAbout = () => {
   return (
@@ -8,88 +23,25 @@ const NewAbout = () => {
       id="about-section"
       sx={{
         marginTop: 10,
-        display: 'flex', // Change to flex to align items
-        alignItems: 'center', // Align items vertically
+        display: 'flex',
+        alignItems: 'center',
         justifyContent: 'center',
         height: '100vh',
         width: '100vw',
         position: 'relative',
         padding: 2,
-        textAlign: 'left', // Left-align text for better structure
-        bgcolor: '#282829', // Add a background color for contrast
+        textAlign: 'left',
+        bgcolor: '#282829',
       }}
     >
-      <img
-        src="/images/person-outline.svg"
-        alt="Person"
-        style={{
-          width: '25vw',
-          height: 'auto',
-          marginRight: '20px', // Add space between image and text
-        }}
-      />
+      <PersonOutlineIcon />
 
       <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
-        <Typography
-          variant="h1"
-          sx={{
-            color: 'white',
-            fontWeight: 'bold',
-            marginBottom: 1, // Optional: space below the title
-          }}
-        >
-          David Riva
-        </Typography>
+        <AboutHeader />
 
-        <Typography
-          variant="h5"
-          sx={{
-            color: '#38c0f2',
-            marginBottom: 1,
-          }}
-        >
-          Technical Trainer
-        </Typography>
+        <Biography />
 
-        <Typography
-          sx={{
-            color: '#757474',
-            marginBottom: 2,
-          }}
-        >
-          Bay Area, CA. |{' '}
-          <a
-            href="mailto:davidjriva@gmail.com"
-            style={{
-              color: '#0a73c9',
-              textDecoration: 'none',
-            }}
-          >
-            davidjriva@gmail.com
-          </a>
-        </Typography>
-
-        <Box sx={{ marginTop: 2, maxWidth: '600px' }}>
-          <Typography variant="body1">
-            Driven and accomplished software engineer and educator with a B.S. in Computer Science from Colorado State
-            University, graduating summa cum laude, and a minor in Mathematics.
-          </Typography>
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            Experienced in full-stack development, data engineering, and big data visualization.
-          </Typography>
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            Proficient in data structures, algorithms, and mathematical applications. Passionate about elegant
-            problem-solving and dedicated to maintaining high standards of excellence.
-          </Typography>
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            Thrives in a collaborative environment that values innovation and rigorous quality.
-          </Typography>
-        </Box>
-
-        <Box sx={{ marginTop: 4, display: 'flex', justifyContent: 'flex-start', gap: 2 }}>
-          <ResumeButton />
-          <SocialLinks />
-        </Box>
+        <AboutFooter />
       </Box>
     </Box>
   );
