baa0d27ae8ee7645508a289475de1c82ecb09d20
David Riva <davidjriva@gmail.com>
2024-10-24 20:45:31 -0700

    Styled education page

--- personal-portfolio/src/components/Education-Page/Education.js
+++ personal-portfolio/src/components/Education-Page/Education.js
@@ -20,15 +20,27 @@ const Education = () => {
     >
       <SectionHeading sectionName="Education" />
 
-      <Box sx={{ alignSelf: 'flex-start', marginBottom: '2rem', width: '100%' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <Box
+        sx={{
+          alignSelf: 'flex-start',
+          marginTop: 10,
+          marginLeft: 10,
+          width: '40%', // Ensure full width for spacing
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between', // Align items to the ends
+            alignItems: 'center', // Center align vertically
+            marginBottom: '1rem', // Increased spacing between section title and dates
+          }}
+        >
           <Typography variant="h5">Bachelor of Science</Typography>
-          <Typography variant="body2" sx={{ textAlign: 'right' }}>
-            August 2021 - May 2024
-          </Typography>
+          <Typography variant="body2">August 2021 - May 2024</Typography>
         </Box>
 
-        <Box sx={{ display: 'flex', alignItems: 'flex-start', marginBottom: '1rem' }}>
+        <Box sx={{ display: 'flex', alignItems: 'flex-start', marginBottom: '1.5rem' }}>
           <img
             src={'/images/CSU_logo.jpeg'}
             alt={'Colorado State University logo'}
@@ -37,13 +49,13 @@ const Education = () => {
           <Typography variant="body1">Colorado State University | Fort Collins, CO</Typography>
         </Box>
 
-        <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>
+        <Typography variant="body1" sx={{ marginBottom: '0.75rem' }}>
           Computer Science, Minor in Mathematics
         </Typography>
-        <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>
+        <Typography variant="body1" sx={{ marginBottom: '0.75rem' }}>
           GPA: 4.0
         </Typography>
-        <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>
+        <Typography variant="body1" sx={{ marginBottom: '0.75rem' }}>
           Summa Cum Laude Distinction
         </Typography>
       </Box>
