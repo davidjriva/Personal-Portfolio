3f13da3d7553a98c3e29b7385f04f1652d5594dd
David Riva <davidjriva@gmail.com>
2024-12-08 13:55:49 -0800

    delete react-app

--- react-app/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from './About/About';
-export { default as Projects } from './Projects/Projects';
-export { default as NavBar } from './NavBar';
-export { default as Experience } from './Experience/Experience';
-export { default as Education } from './Education/Education';
-export { default as Awards } from './Awards/Awards';
-export { default as Skills } from './Skills/Skills';
