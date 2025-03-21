8063c49ace03cd10544ef802e9a25f1b00e09748
David Riva <davidjriva@gmail.com>
2025-03-20 22:09:01 -0700

    Allow scrolling of card list by clicking cards

--- next-app/src/components/Projects-Page/ProjectsContainer.js
+++ next-app/src/components/Projects-Page/ProjectsContainer.js
@@ -1,8 +1,21 @@
+'use client';
+
 import ProjectCards from './ProjectCards';
 import projectData from '../../data/projects.json';
 import { Box } from '@mui/material';
 
 const ProjectsContainer = () => {
+  const scrollToCenter = (index) => {
+    const cardElement = document.getElementById(`project-card-${index}`);
+    if (cardElement) {
+      cardElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center',
+      });
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -16,7 +29,7 @@ const ProjectsContainer = () => {
         '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#555' },
       }}
     >
-      <ProjectCards projects={projectData} />
+      <ProjectCards projects={projectData} onClick={scrollToCenter} />
     </Box>
   );
 };
