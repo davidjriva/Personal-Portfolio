fe95106de5e2b797c431a59835c20caba47a457d
David Riva <davidjriva@gmail.com>
2025-03-20 22:44:53 -0700

    Rewrite SectionHeading to use MUI not raw HTML

--- next-app/src/components/SectionHeading.js
+++ next-app/src/components/SectionHeading.js
@@ -1,29 +1,28 @@
+import { Typography } from '@mui/material';
+
 const SectionHeading = ({ sectionName }) => {
   return (
-    <h2
-      style={{
-        marginTop: '0px',
+    <Typography
+      variant="h2"
+      sx={{
+        marginTop: 0,
         marginBottom: '80px',
         textShadow: '3px 3px #011627',
-        fontFamily: 'sans-serif',
         color: '#FFF',
         fontSize: '3rem',
-      }}
-    >
-      <span
-        style={{
+        '& span': {
           display: 'inline-block',
           backgroundSize: '100% 1.2em',
           backgroundPosition: '0.5em 0px',
           backgroundRepeat: 'no-repeat space',
           paddingRight: '.8em',
           marginRight: '-0.5em',
-          backgroundImage: 'linear-gradient(transparent 55%, #38c0f2  55%, #38c0f2 95%, transparent 95%)',
-        }}
-      >
-        {sectionName}
-      </span>
-    </h2>
+          backgroundImage: 'linear-gradient(transparent 55%, #38c0f2 55%, #38c0f2 95%, transparent 95%)',
+        },
+      }}
+    >
+      <span>{sectionName}</span>
+    </Typography>
   );
 };
 
