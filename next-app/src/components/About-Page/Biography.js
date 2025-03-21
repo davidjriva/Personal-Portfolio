02ff46a84f4fa8cb8c236d654a8a2ef5965578f0
David Riva <davidjriva@gmail.com>
2025-03-20 22:55:29 -0700

    cleanup duplicated code for font-sizing

--- next-app/src/components/About-Page/Biography.js
+++ next-app/src/components/About-Page/Biography.js
@@ -1,10 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import ClickableLink from '@/components/About-Page/ClickableLink';
 
-const biographyTypographyStyles = { 
-  fontSize: '1rem'
-};
-
 const Biography = () => {
   return (
     <Box
@@ -14,15 +10,15 @@ const Biography = () => {
         textAlign: 'left',
       }}
     >
-      <Typography variant="body1" sx={...biographyTypographyStyles}>
+      <Typography variant="body1">
         Hi, I'm David. I'm a Technical Trainer @ <ClickableLink link="https://c3.ai" company="C3 AI" /> based in the Bay
         Area, CA, and a graduate of Colorado State University, where I received a B.S. in Computer Science with Summa
         Cum Laude distinctions. I'm incredibly passionate about software engineering & UI / UX design.
       </Typography>
-      <Typography variant="body1" sx={{ marginTop: 2, ...biographyTypographyStyles }}>
+      <Typography variant="body1" sx={{ marginTop: 2 }}>
         I'm experienced in full-stack development, data engineering, and big data visualization.
       </Typography>
-      <Typography variant="body1" sx={{ marginTop: 2, ...biographyTypographyStyles }}>
+      <Typography variant="body1" sx={{ marginTop: 2 }}>
         I have a strong background in data structures, algorithms, and mathematical applications. I pride myself on
         elegant problem-solving and my dedication to maintaining high standards of excellence.
       </Typography>
