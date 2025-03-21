a4aed10094b2575e21d43f3f4079f221787c281a
David Riva <davidjriva@gmail.com>
2025-03-20 22:54:01 -0700

    increase font-sizing on biography

--- next-app/src/components/About-Page/Biography.js
+++ next-app/src/components/About-Page/Biography.js
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material';
 import ClickableLink from '@/components/About-Page/ClickableLink';
 
 const biographyTypographyStyles = { 
-  fontSize: { xs: '0.75rem', sm: '1rem', md: '1rem' }
+  fontSize: '1rem'
 };
 
 const Biography = () => {
