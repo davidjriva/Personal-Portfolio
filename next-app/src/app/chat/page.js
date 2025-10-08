4b4da6fb54d95dd64517d67097a24bbc9b83a358
David Riva <davidjriva@gmail.com>
2025-10-07 20:09:45 -0700

    Make ParticleBackground not interactive for chat

--- next-app/src/app/chat/page.js
+++ next-app/src/app/chat/page.js
@@ -3,11 +3,10 @@ import { Box } from '@mui/material';
 import ParticleBackground from '@/components/ParticleBackground';
 import ChatContainer from '@/components/Chat-Page/ChatContainer';
 
-// --------------------- ChatPage ---------------------
 export default function ChatPage() {
   return (
     <Box sx={{ position: 'relative' }}>
-      <ParticleBackground />
+      <ParticleBackground interactive={false} />
       <ChatContainer />
     </Box>
   );
