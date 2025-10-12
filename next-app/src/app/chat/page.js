863ad72c0962bd6e7e75137f1e3c6f83e3c6e151
David Riva <davidjriva@gmail.com>
2025-10-12 13:27:39 -0700

    Fix particle background slingshotting

--- next-app/src/app/chat/page.js
+++ next-app/src/app/chat/page.js
@@ -5,8 +5,19 @@ import ChatContainer from '@/components/Chat-Page/ChatContainer';
 
 export default function ChatPage() {
   return (
-    <Box sx={{ position: 'relative' }}>
-      <ParticleBackground interactive={false} />
+    <Box sx={{ position: 'relative', backgroundColor: '#282829' }}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 0,
+        }}
+      >
+        <ParticleBackground interactive={false} />
+      </Box>
       <ChatContainer />
     </Box>
   );
